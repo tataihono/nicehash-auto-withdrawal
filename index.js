cd3651c6ad46a365995b59e4122aa41aeec4c569
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-03-16 17:33:37 +1300

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { default: NiceHash } = require("nicehash-api");
+
+async function main() {
+  try {
+    const instance = new NiceHash({
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET,
+      api_id: process.env.API_ID,
+    });
+    await instance.testAuthorization();
+    const balance = await instance.getBalance();
+    if (balance > 0) {
+      instance.createWithdrawlRequest(
+        process.env.WITHDRAWAL_ID,
+        balance,
+        "BTC"
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
